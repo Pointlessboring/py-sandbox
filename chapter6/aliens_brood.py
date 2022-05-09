a9e1a8c50d36680052fa6c692f7cd5e83532a4f8
Pointlessboring <pointlessboring@gmail.com>
2022-05-09 06:52:52 -0400

    Updated Ch6

---
+++ chapter6/aliens_brood.py
@@ -0,0 +1,27 @@
+aliens = []
+
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points':5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens [:5]:
+    print(alien)
+print("...")
+print(f"Total number of aliens is: {len(aliens)}")
+
+print("They are changing!!!")
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['points'] = 10
+        alien['speed'] = 'medium'
+    if alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['points'] = 15
+        alien['speed'] = 'fast'
+
+for alien in aliens [:5]:
+    print(alien)
+print("...")
+print(f"Total number of aliens is: {len(aliens)}")
---
+++ chapter6/pizza.py
@@ -0,0 +1,9 @@
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushroom', 'extra cheese'],
+}
+
+print("\n")
+print(f"You ordered a {pizza['crust']}-crust pizza with the following toppings:")
+for topping in pizza['toppings']:
+    print(f"\t{topping}")
