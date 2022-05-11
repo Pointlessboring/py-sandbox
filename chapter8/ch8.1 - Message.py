09811b5a42d3491bd95d0146e62a0498023cecad
Pointlessboring <pointlessboring@gmail.com>
2022-05-11 07:46:21 -0400

    Updated Progress Ch.8

---
+++ chapter8/ch8.1 - Message.py
@@ -0,0 +1,10 @@
+from dis import dis
+
+
+def display_msg(message):
+    """Simple printing function"""
+    print(f"{message}")
+
+display_msg("This is a simple program.")
+display_msg("It uses a function call to print strings.")
+display_msg("Hope you like it!")
