20ec4033791f0fa35783ca2befbdbe33a75290de
Pointlessboring <pointlessboring@gmail.com>
2022-05-28 06:27:56 -0400

    Added Ch17, github API examples

--- data_visualization/chapter17/python_repos.py
+++ data_visualization/chapter17/python_repos.py
@@ -1,4 +1,3 @@
-from email import header
 import requests
 
 # Make API call and store the response.
--- data_visualization/chapter17/python_repos_v2.py
+++ data_visualization/chapter17/python_repos_v2.py
@@ -1,4 +1,3 @@
-from email import header
 import requests
 
 # Make API call and store the response.
@@ -16,18 +15,14 @@ print(f"Total repositories: {response_dict['total_count']}")
 repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
 
-# Examine the firsst repository.
-repo_dict = repo_dicts[0]
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
+    print("\n")
 
-print("\nSelected information about the first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
-
-# print(f"\nKeys: {len(repo_dict)}")
-# for key in sorted(repo_dict.keys()):
-#    print(key)
