bab29fcdfc635a133b549fb14fa2862062539734
PointlessBoring <PointlessBoring@gmail.com>
2023-02-12 17:07:01 -0500

    P10 - early beginnings

---
+++ BBSP_mycode/P10/Step1.py
@@ -0,0 +1,16 @@
+"""Cho-Han, inspired from Al Sweigart al@inventwithpython.com
+The traditional Japanese dice game of even-odd."""
+
+import random, sys
+
+JAPANESE_NUMBERS = {1: 'ICHI', 2: 'NI', 3: 'SAN',
+                    4: 'SHI', 5: 'GO', 6: 'ROKU'}
+
+print('''Cho-Han, by Al Sweigart al@inventwithpython.com
+
+In this traditional Japanese dice game, two dice are rolled in a bamboo
+cup by the dealer sitting on the floor. The player must guess if the
+dice total to an even (cho) or odd (han) number.
+''')
+
+
